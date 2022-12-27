29b782ec4fbc24d7f269e88084271184ef51cab4
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-12-27 02:35:08 +0000

    [fix] Enable filtering by title

--- backend/app/api/dependencies/items.py
+++ backend/app/api/dependencies/items.py
@@ -21,12 +21,14 @@ from app.services.items import check_user_can_modify_item
 def get_items_filters(
     tag: Optional[str] = None,
     seller: Optional[str] = None,
+    title: Optional[str] = None,
     favorited: Optional[str] = None,
     limit: int = Query(DEFAULT_ITEMS_LIMIT, ge=1),
     offset: int = Query(DEFAULT_ITEMS_OFFSET, ge=0),
 ) -> ItemsFilters:
     return ItemsFilters(
         tag=tag,
+        title=title,
         seller=seller,
         favorited=favorited,
         limit=limit,
